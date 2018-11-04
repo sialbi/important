7b63090ee22174e30d5f1cc4d3aee00941f71fae
sialbi <sialbi@users.noreply.github.com>
2018-11-04 17:43:29 +0100

    third commit

---
+++ R/ilpkgs.R
@@ -0,0 +1,9 @@
+# ipak function: install and load multiple R packages.
+# check to see if packages are installed. Install them if they are not, then load them into the R session.
+
+ilpkgs <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
