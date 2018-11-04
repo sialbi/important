fab8bbaeb35ae6e68bff22959618ae18737eddbb
sialbi <sialbi@users.noreply.github.com>
2018-11-04 19:53:58 +0100

    test

--- R/square_foo.R
+++ R/square_foo.R
@@ -1,24 +1,10 @@
-# Development an R Package
+# Square function
 
-system("R CMD build glm.predict")
-
-system("R CMD check glm.predict_3.0-4.tar.gz")
-
-system("R CMD check glm.predict_3.0-4.tar.gz --as-cran")
-
-## Check an R package ##
-# wrong version #
-
-system("R CMD build glm.predict_bad")
-
-system("R CMD build glm.predict_bad")
-
-system("R CMD check glm.predict_4.0.tar.gz")
-
-system("R CMD build glm.predict_bad")
-system("R CMD check glm.predict_4.0.tar.gz")
-
-## Install the package ##
-system("R CMD INSTALL glm.predict_3.0-4.tar.gz")
-
-devtools::install_github("benjaminschlegel/glm.predict")
+square_function <- function(value){
+  if (is.numeric(value)) {
+    square = value * value
+    cat("The square of", value, "is equal to", square)}
+  else {
+    cat("The value '",value,"'", "has to be numeric to calculate the square")
+  }
+}
